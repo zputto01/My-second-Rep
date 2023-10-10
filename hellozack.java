15a0a8820eba4b99ef930370c3c46ac3abe75a08
zputto01 <147326025+zputto01@users.noreply.github.com>
2023-10-10 18:49:01 +0100

    Just wanted to say hello to Zack

---
+++ hellozack.java
@@ -0,0 +1,9 @@
+public class hellozack {
+        static public void main(String[] args) {
+    
+            System.out.println("Hey zack");
+            System.out.print(2.3f + 4.5f);
+        }
+    }
+    
+
