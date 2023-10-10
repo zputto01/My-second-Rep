eb9246d6478b6377ca2d81932d4a5184d60b7eeb
zputto01 <147326025+zputto01@users.noreply.github.com>
2023-10-10 19:23:39 +0100

    The code was buggy - Revert "Some extra independent added code"
    This reverts commit 858be67c42c251412507f27e4cf871c546951598.

--- hellozack.java
+++ hellozack.java
@@ -1,9 +1,8 @@
 public class hellozack {
         static public void main(String[] args) {
     
-            java.utli.Scanner scan = new java.util.Scanner(System.in); System.out.println("Hello World!"); System.out.println("What’s your name?");
-String s = scan.nextLine();
-        System.out.println("Hello " + s + "!");
+            System.out.println("Hey zack");
+            System.out.print(2.3f + 4.5f);
         }
     }
     
